196fb9d1c23427a78a95a12866062e47fb42df24
fatadel <fatadel@gmail.com>
2019-02-23 16:23:01 +0300

    Extracted constants to separate file

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
 import requests
 import sys
+import constants
 
 from time import sleep
 from datetime import datetime
 
-HTTP_SUCCESS = 200
-SLEEP_TIME = 5
-SECONDS_IN_HOUR = 3600
-SECONDS_IN_MINUTE = 60
-
 # Check proper usage
 if len(sys.argv) != 2:
-    print('Usage: python app.py N, where N is a UNIX timestamp value for HardFork time')
+    print(constants.errors['usage_error'])
     sys.exit(1)
 
 # Get the timestamp
 try:
     timestamp = datetime.utcfromtimestamp(int(sys.argv[1]))
 except ValueError:
-    print('Usage: python app.py N, where N is a UNIX timestamp value for HardFork time')
+    print(constants.errors['usage_error'])
     sys.exit(1)
 
 while True:
     try:
         response = requests.get('https://api.blockchair.com/bitcoin-cash/blocks?limit=1&s=median_time(desc)')
     except requests.exceptions.ConnectionError:
-        print('Service not available')
-        sleep(SLEEP_TIME)
+        print(constants.errors['service_error'])
+        sleep(constants.SLEEP_TIME)
         continue
     response_data = response.json()
-    if response.status_code == HTTP_SUCCESS:
+    if response.status_code == requests.codes.ok:
         median_time = datetime.strptime(response_data.get('data')[0].get('median_time'), '%Y-%m-%d %H:%M:%S')
         if median_time >= timestamp:
             print('HardFork happened!')
@@ -37,16 +33,17 @@ while True:
         else:
             dt = timestamp - median_time
             days = '{} day(s) '.format(dt.days) if dt.days > 0 else ''
-            hours = '{} hour(s) '.format(dt.seconds // SECONDS_IN_HOUR) if dt.seconds // SECONDS_IN_HOUR > 0 else ''
-            minutes = '{} minute(s) '.format(dt.seconds % SECONDS_IN_HOUR // SECONDS_IN_MINUTE) \
-                if dt.seconds % SECONDS_IN_HOUR // SECONDS_IN_MINUTE > 0 else ''
-            seconds = '{} second(s)'.format(dt.seconds % SECONDS_IN_HOUR % SECONDS_IN_MINUTE) \
-                if dt.seconds % SECONDS_IN_HOUR % SECONDS_IN_MINUTE > 0 else ''
+            hours = dt.seconds // constants.SECONDS_IN_HOUR
+            hours = '{} hour(s) '.format(hours) if hours > 0 else ''
+            minutes = dt.seconds % constants.SECONDS_IN_HOUR // constants.SECONDS_IN_MINUTE
+            minutes = '{} minute(s) '.format(minutes) if minutes > 0 else ''
+            seconds = dt.seconds % constants.SECONDS_IN_HOUR % constants.SECONDS_IN_MINUTE
+            seconds = '{} second(s)'.format(seconds) if seconds > 0 else ''
             print('HardFork will happen in {}{}{}{}'.format(days, hours, minutes, seconds))
     else:
         error = response_data.get('context').get('error')
         if error:
             print(error)
         else:
-            print('Service not available')
-    sleep(SLEEP_TIME)
+            print(constants.errors['service_error'])
+    sleep(constants.SLEEP_TIME)
---
+++ constants.py
@@ -0,0 +1,8 @@
+SLEEP_TIME = 5
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+
+errors = {
+    'usage_error': 'Usage: python app.py N, where N is a UNIX timestamp value for HardFork time',
+    'service_error': 'Service not available'
+}
