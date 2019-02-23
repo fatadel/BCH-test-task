6fddc68ffa8917baecb22597229485652ef6fe24
fatadel <fatadel@gmail.com>
2019-02-23 17:28:10 +0300

    Commented code in app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 # Get the timestamp
+# Covert it from UNIX timestamp to UTC datetime object
 try:
     timestamp = datetime.utcfromtimestamp(int(sys.argv[1]))
 except ValueError:
@@ -18,19 +19,26 @@ except ValueError:
     sys.exit(1)
 
 while True:
+    # Get the most recent MTP-11
     try:
         response = requests.get('https://api.blockchair.com/bitcoin-cash/blocks?limit=1&s=median_time(desc)')
     except requests.exceptions.ConnectionError:
         print(constants.errors['service_error'])
         sleep(constants.SLEEP_TIME)
         continue
+
+    # Decode response
     response_data = response.json()
+
     if response.status_code == requests.codes.ok:
+        # Convert string-formatted datetime to UTC datetime object
         median_time = datetime.strptime(response_data.get('data')[0].get('median_time'), '%Y-%m-%d %H:%M:%S')
+
         if median_time >= timestamp:
             print('HardFork happened!')
             sys.exit(0)
         else:
+            # Calculate time to HardFork
             dt = timestamp - median_time
             days = '{} day(s) '.format(dt.days) if dt.days > 0 else ''
             hours = dt.seconds // constants.SECONDS_IN_HOUR
@@ -39,6 +47,7 @@ while True:
             minutes = '{} minute(s) '.format(minutes) if minutes > 0 else ''
             seconds = dt.seconds % constants.SECONDS_IN_HOUR % constants.SECONDS_IN_MINUTE
             seconds = '{} second(s)'.format(seconds) if seconds > 0 else ''
+
             print('HardFork will happen in {}{}{}{}'.format(days, hours, minutes, seconds))
     else:
         error = response_data.get('context').get('error')
@@ -46,4 +55,6 @@ while True:
             print(error)
         else:
             print(constants.errors['service_error'])
+
+    # Sleep SLEEP_TIME seconds until next try
     sleep(constants.SLEEP_TIME)
