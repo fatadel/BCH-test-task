1616bd02605dc38b09a0a32d3acbcdc0822e75fc
fatadel <fatadel@gmail.com>
2019-02-23 17:31:32 +0300

    Commented code in constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,11 @@
+# Sleep time between each request (in seconds)
 SLEEP_TIME = 5
+# Amount of seconds in one hour
 SECONDS_IN_HOUR = 3600
+# Amount of seconds in one minute
 SECONDS_IN_MINUTE = 60
 
+# Dictionary of available error messages
 errors = {
     'usage_error': 'Usage: python app.py N, where N is a UNIX timestamp value for HardFork time',
     'service_error': 'Service not available'
